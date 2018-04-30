450ea3faa0c5ba97fb6baec8a49acc71da11c36b
Nico Zink <nico@nfzink.com>
2018-04-30 09:12:19 +0100

    Use cmake folders, and add assert class.

---
+++ include/cpp_util/debug/assert.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Nico Zink
+ * https://github.com/nicozink/cpp_util.git
+ * Licensed under GNU General Public License 3.0 or later. 
+ */
+
+#pragma once
+
+#ifndef cpp_util_debug_assert_h
+#define cpp_util_debug_assert_h
+
+// Local Includes
+
+// Project Includes
+
+// External Includes
+#include <cassert>
+
+// Macro Definitions
+
+// Asserts the condition, and prints the error otherwise.
+#define Assert(condition, message) \
+  assert(condition && message);
+#endif
---
+++ src/debug/assert.cpp
@@ -0,0 +1,14 @@
+/*
+Copyright © 2012, Nico Zink
+All rights reserved.
+*/
+
+#include <cpp_util/debug/assert.h>
+
+// Local Includes
+
+// Project Includes
+
+// System Includes
+
+// This is a macro definition included in the header file.
